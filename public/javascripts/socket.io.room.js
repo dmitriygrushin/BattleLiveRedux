47bb9e41c76b3cfa0a3805a15ac7f642a9d06a4b
Dmitriy Grushin <dmitriy_grushin@yahoo.com>
2022-06-26 12:47:23 -0400

    Refactored client-side & added server-side webRTC code

--- public/javascripts/socket.io.room.js
+++ public/javascripts/socket.io.room.js
@@ -1,4 +1,6 @@
-const socket = io();
+let socket; //= io();
+let localStream = null;
+let peers = {};
 
 const messages = document.getElementById('messages');
 const form = document.getElementById('form');
@@ -11,10 +13,32 @@ listUsers.style.display = 'none';
 
 const chatUserListToggle = document.getElementById('flexSwitchCheckChecked');
 
-socket.emit('join-room', roomId, userId);
+init();
 
-// get list of users in room
-socket.on('update-user-list', users => {
+
+/**
+ * initialize socket connections
+ */
+function init() {
+    socket = io();
+
+    socket.emit('join-room', roomId, userId);
+
+    // get list of users in room
+    socket.on('update-user-list', users => { onUpdateUserList(users) });
+
+    
+    /**
+     * Chat
+     */
+    chatUserListToggle.addEventListener('click', chatUserListToggleVisibility); // toggle div visibility
+    chatUserListToggle.addEventListener('click', chatUserListSwitchButton);
+    form.addEventListener('submit', onChatFormSubmit);
+    // send message to server
+    socket.on('chat-message', msg => { onChatMessage(msg) });
+}
+
+function onUpdateUserList(users) {
     // clear ul list
     listUsers.querySelectorAll('*').forEach(n => {n.remove()});
 
@@ -24,10 +48,9 @@ socket.on('update-user-list', users => {
         li.textContent = user.username;
         listUsers.appendChild(li);
     });
-});
+}
 
-// toggle div visibility
-chatUserListToggle.addEventListener('click', () => {
+function chatUserListToggleVisibility() {
     if (chatUserListToggle.checked) {
         listChat.style.display = 'block';
         listUsers.style.display = 'none';
@@ -37,26 +60,24 @@ chatUserListToggle.addEventListener('click', () => {
         listUsers.style.display = 'block';
     }
 }
-);
 
-chatUserListToggle.addEventListener('click', () => {
+function chatUserListSwitchButton() {
     const chatUserListText = document.getElementById('chatUserListText');
     chatUserListText.innerHTML = (chatUserListToggle.checked) ? 'Chat' : 'User List';
-});
+}
 
-form.addEventListener('submit', (e) => {
+function onChatFormSubmit(e) {
     e.preventDefault();
     if (input.value) {
         let message = `${username}: ${input.value}`;
         socket.emit('chat-message', message);
         input.value = '';
     }
-});
-
-// send message to server
-socket.on('chat-message', (msg) => {
+}
+ 
+function onChatMessage(msg) {
     const item = document.createElement('li');
     item.textContent = msg;
     messages.appendChild(item);
     scrollDiv.scrollTo(0, document.body.scrollHeight); // make the chat scroll to the bottom
-});
+}
