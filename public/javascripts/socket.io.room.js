b8646a590e87e1c008270bf513cf5f3f56fbc08a
Dmitriy Grushin <dmitriy_grushin@yahoo.com>
2022-06-27 12:24:32 -0400

    Implemented WebRTC

--- public/javascripts/socket.io.room.js
+++ public/javascripts/socket.io.room.js
@@ -1,23 +1,84 @@
-let socket; //= io();
+let socket; // = io();
 let localStream = null;
 let peers = {};
 
+    /* --------------- Chat --------------- */
 const messages = document.getElementById('messages');
 const form = document.getElementById('form');
 const input = document.getElementById('input');
-
 const scrollDiv = document.getElementById('scroll-div-chat');
 const listChat = document.getElementById('messages');
 const listUsers = document.getElementById('user-list');
 listUsers.style.display = 'none';
-
 const chatUserListToggle = document.getElementById('flexSwitchCheckChecked');
+    /* ------------------------------ */
+
+    /* --------------- WebRTC --------------- */
+/**
+ * RTCPeerConnection Configuration
+ */
+const configuration = {
+    "iceServers": [
+    {
+      urls: "stun:openrelay.metered.ca:80"
+    },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    }
+  ]
+}
+
+/**
+ * UserMedia constrains
+ */
+const constraints = {
+    audio: true,
+    video: {
+        width: {
+            max: 300
+        },
+        height: {
+            max: 300
+        }
+    }
+}
+
+constraints.video.facingMode = {
+    ideal: "user"
+}
+
+/** 
+ * Enabling the camera at startup 
+ */ 
+navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+    console.log('Received local stream');
+
+    localVideo.srcObject = stream;
+    localStream = stream;
+
+    init()
+
+}).catch(e => alert(`getusermedia error ${e.name}`))
+    /* ------------------------------ */
+
 
 init();
 
 
 /**
- * initialize socket connections
+ * Initialize socket connections
  */
 function init() {
     socket = io();
@@ -27,17 +88,129 @@ function init() {
     // get list of users in room
     socket.on('update-user-list', users => { onUpdateUserList(users) });
 
+
+    /* --------------- WebRTC --------------- */
+    socket.on('initReceive', socket_id => {
+        console.log('INIT RECEIVE ' + socket_id)
+        addPeer(socket_id, false)
+        socket.emit('initSend', socket_id)
+    })
+
+    socket.on('initSend', socket_id => {
+        console.log('INIT SEND ' + socket_id)
+        addPeer(socket_id, true)
+    })
+
+    socket.on('removePeer', socket_id => {
+        console.log('removing peer ' + socket_id)
+        removePeer(socket_id)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('GOT DISCONNECTED')
+        for (let socket_id in peers) {
+            removePeer(socket_id)
+        }
+    })
+
+    socket.on('signal', data => {
+        peers[data.socket_id].signal(data.signal)
+    })
+    /* ------------------------------ */
+
     
-    /**
-     * Chat
-     */
+    /* --------------- Chat --------------- */
     chatUserListToggle.addEventListener('click', chatUserListToggleVisibility); // toggle div visibility
     chatUserListToggle.addEventListener('click', chatUserListSwitchButton);
     form.addEventListener('submit', onChatFormSubmit);
     // send message to server
     socket.on('chat-message', msg => { onChatMessage(msg) });
+    /* ------------------------------ */
 }
 
+/* --------------- WebRTC --------------- */
+/**
+ * Remove a peer with given socket_id. 
+ * Removes the video element and deletes the connection
+ * @param {String} socket_id 
+ */
+function removePeer(socket_id) {
+
+    let videoEl = document.getElementById(socket_id)
+    if (videoEl) {
+
+        const tracks = videoEl.srcObject.getTracks();
+
+        tracks.forEach(function (track) {
+            track.stop()
+        })
+
+        videoEl.srcObject = null
+        videoEl.parentNode.removeChild(videoEl)
+    }
+    if (peers[socket_id]) peers[socket_id].destroy()
+    delete peers[socket_id]
+}
+
+/**
+ * Creates a new peer connection and sets the event listeners
+ * @param {String} socket_id 
+ *                 ID of the peer
+ * @param {Boolean} am_initiator 
+ *                  Set to true if the peer initiates the connection process.
+ *                  Set to false if the peer receives the connection. 
+ */
+function addPeer(socket_id, am_initiator) {
+    peers[socket_id] = new SimplePeer({
+        initiator: am_initiator,
+        stream: localStream,
+        config: configuration
+    })
+
+    peers[socket_id].on('signal', data => {
+        socket.emit('signal', {
+            signal: data,
+            socket_id: socket_id
+        })
+    })
+
+    // append stream to video element
+    peers[socket_id].on('stream', stream => {
+        let newVid = document.createElement('video')
+        newVid.srcObject = stream
+        newVid.id = socket_id
+        newVid.playsinline = false
+        newVid.autoplay = true
+        newVid.className = "vid"
+        newVid.onclick = () => openPictureMode(newVid)
+        newVid.ontouchstart = (e) => openPictureMode(newVid)
+        videos.appendChild(newVid)
+    })
+}
+
+/**
+ * Opens an element in Picture-in-Picture mode
+ * @param {HTMLVideoElement} el video element to put in pip mode
+ */
+function openPictureMode(el) {
+    console.log('opening pip')
+    el.requestPictureInPicture()
+}
+
+/**
+ * updating text of buttons
+ */
+function updateButtons() {
+    for (let index in localStream.getVideoTracks()) {
+        vidButton.innerText = localStream.getVideoTracks()[index].enabled ? "Video Enabled" : "Video Disabled"
+    }
+    for (let index in localStream.getAudioTracks()) {
+        muteButton.innerText = localStream.getAudioTracks()[index].enabled ? "Unmuted" : "Muted"
+    }
+}
+/* ------------------------------ */
+
+/* --------------- Chat --------------- */
 function onUpdateUserList(users) {
     // clear ul list
     listUsers.querySelectorAll('*').forEach(n => {n.remove()});
@@ -81,3 +254,6 @@ function onChatMessage(msg) {
     messages.appendChild(item);
     scrollDiv.scrollTo(0, document.body.scrollHeight); // make the chat scroll to the bottom
 }
+/* ------------------------------ */
+
+
