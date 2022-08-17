5b047c34a50dd6581f66087df9d34d190e4970f3
Dmitriy Grushin <dmitriy_grushin@yahoo.com>
2022-08-17 18:02:45 -0400

    Added: simple count down

--- utilities/socket.io/db.js
+++ utilities/socket.io/db.js
@@ -70,3 +70,104 @@ module.exports.isInQueueAndNotRapper = async (roomId, userId) => {
     console.log('Current User:' + rows);
     return rows.length > 0;
 }
+
+
+/**
+ * rapRoomEventLoopController.js DB functions
+ */
+
+/**
+ * Check if the room is a rap room
+ */
+module.exports.isRapRoom = async (roomId) => {
+    const { rows } = await pool.query( `SELECT is_rap_room FROM room WHERE id = $1`, [roomId]);
+    return rows[0].is_rap_room;
+}
+
+/**
+ * Check the room requirements for a room to become a rap room
+ */
+module.exports.checkRoomRequirements = async (roomId) => {
+    const usersInQueue = 2;
+    const totalUsers = 2;
+
+    /**
+     * Production room requirements: 2 rappers in_queue, 10 users total
+     * Testing room requirements: 2 rappers in_queue, 2 users total
+        `SELECT user_account.username, room.id AS room_id, room.description, room.user_id 
+        FROM user_account JOIN room ON user_account.id = room.user_id 
+        WHERE room.id = $1`, [roomId]
+     */
+
+    // select all user_connected in the room
+    const { rows } = await pool.query(
+        `SELECT user_connected.id AS user_id,
+        user_connected.room_id as room_id,
+        user_connected.is_rapper as is_rapper,
+        user_connected.in_queue as in_queue
+        FROM user_connected
+        WHERE room_id = $1`, [roomId]);
+    console.log('Current Users:' + rows);
+    // if the room has less than 2 rappers in_queue, return false
+
+    let isEnoughInQueue = rows.filter(user => user.in_queue == true).length >= usersInQueue;  
+    let isEnoughUsers = rows.length >= totalUsers;
+
+    return isEnoughInQueue && isEnoughUsers;
+}
+
+module.exports.chooseRappers = async (roomId) => {
+    // out of the rappers in the room who are in_queue, choose 2 to be rappers update is_rapper to true
+    const { rows } = await pool.query(
+        `SELECT user_connected.id AS user_id,
+        user_connected.room_id as room_id,
+        user_connected.is_rapper as is_rapper,
+        user_connected.in_queue as in_queue
+        FROM user_connected
+        WHERE room_id = $1 AND in_queue = true
+        LIMIT 2`, [roomId]);
+
+    // if the room has less than 2 rappers in_queue, return false
+    if (rows.length < 2) {
+        return false;
+    }
+
+    // update is_rapper to true for the rappers in_queue
+    await pool.query(
+        `UPDATE user_connected SET is_rapper = true
+        WHERE id = $1 AND room_id = $2 AND in_queue = true`, [rows[0].user_id, roomId]);
+    await pool.query(
+        `UPDATE user_connected SET is_rapper = true
+        WHERE id = $1 AND room_id = $2 AND in_queue = true`, [rows[1].user_id, roomId]);
+
+    return true;
+}
+
+module.exports.rappersReady = async (roomId) => {
+    const { rows } = await pool.query(
+        `SELECT user_connected.id AS user_id,
+        user_connected.room_id as room_id,
+        user_connected.is_rapper as is_rapper,
+        user_connected.in_queue as in_queue
+        FROM user_connected
+        WHERE room_id = $1 AND is_rapper = true AND in_queue = true AND is_ready = true`, [roomId]);
+    console.log('Current Users:' + rows);
+    return rows.length > 0;
+}
+
+module.exports.makeRapRoom = async (roomId) => {
+    await pool.query( `UPDATE room SET is_rap_room = true WHERE id = $1`, [roomId]);
+    return true;
+}
+
+module.exports.rappersBeenChosen = async (roomId) => {
+    // check if 2 rappers are in_queue and are rappers
+    const { rows } = await pool.query(
+        `SELECT user_connected.id AS user_id,
+        user_connected.room_id as room_id,
+        user_connected.is_rapper as is_rapper,
+        user_connected.in_queue as in_queue
+        FROM user_connected
+        WHERE room_id = $1 AND is_rapper = true AND in_queue = true`, [roomId]);
+    return rows.length == 2;
+}
--- utilities/socket.io/rapEventLoopController.js
+++ utilities/socket.io/rapEventLoopController.js
@@ -1,6 +1,7 @@
-const { addUserToQueue, makeRapper, isInQueueAndNotRapper } = require('./db');
+const { addUserToQueue, makeRapper, isInQueueAndNotRapper, isRapRoom, 
+    checkRoomRequirements, rappersReady, chooseRappers, makeRapRoom, rappersBeenChosen } = require('./db');
+
 module.exports.rapEventLoopController = async (io, socket, roomId) => {
-    socket.on('add-user-to-queue', async (roomId, userId) => { await addUserToQueue(roomId, userId) });
 
     // update user in user_connected table to is_rapper to true if they are in_queue
     socket.on('become-rapper', async (roomId, userId) => {
@@ -14,4 +15,50 @@ module.exports.rapEventLoopController = async (io, socket, roomId) => {
         // signal all users to allow the stream to be displayed
         socket.broadcast.to(roomId).emit('give-broadcast-permission', socket.id); 
     });
+
+    /**
+     * Event Loop
+     */
+    socket.on('add-user-to-queue', async (roomId, userId) => { 
+        await addUserToQueue(roomId, userId);
+        await rapRoomEventLoop(io, socket, roomId);
+    });
+
+    await rapRoomEventLoop(io, socket, roomId);
+}
+
+async function rapRoomEventLoop(io, socket, roomId) {
+    // check if not a rap room. If not, make it a rap room
+    //io.to(roomId).emit('chat-message', '0');
+    if (!(await isRapRoom(roomId))) {
+        //io.to(roomId).emit('chat-message', '1');
+        if (await checkRoomRequirements(roomId)) {
+            //io.to(roomId).emit('chat-message', '2');
+            // if 2 rappers have not already been chosen, then choose 2 rappers
+            if (!(await rappersBeenChosen(roomId))) {
+                //io.to(roomId).emit('chat-message', '3');
+                await chooseRappers(roomId);  
+            }
+
+            // check if rappers are ready
+            if (!(await rappersReady(roomId))) { // negated for testing purposes
+                //io.to(roomId).emit('chat-message', '4');
+                await makeRapRoom(roomId);
+                await startTimers(io, socket, roomId);
+            }
+        }
+    }
+}
+
+async function startTimers(io, socket, roomId) {
+    countDown(io, socket, roomId, 10)
+}
+
+function countDown(io, socket, roomId, seconds) {
+    let timer = setInterval(() => {
+        seconds--;
+        io.to(roomId).emit('chat-message', seconds);
+        console.log('seconds: ' + seconds);
+        if(seconds == 0) clearInterval(timer);
+    }, 1000);
 }
